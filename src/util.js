ffc5433d8b7e6a05e5e84fd488ad03ae29c21ed4
hashrock <hashedrock@gmail.com>
2017-03-19 03:43:53 +0900

    feat(util): Markdown変換機能の実装

--- src/util.js
+++ src/util.js
@@ -9,7 +9,7 @@
  * 真面目に対応せず、なんとなくネストっぽい見た目になっていればいいやということで、
  * リスト記号の前についてるスペースをそのまま残すことにした。
  */
-export function precompileList(input){
+function precompileList(input){
   var list = input.split("\n")
   var convertedList = list.map(line => {
     var pattern = /^( *)[-*+] /
@@ -24,4 +24,42 @@ export function precompileList(input){
 }
 
 
+function scrapboxToMarkdown(input){
+  var patterns = [
+//    {name: "blockquote", pattern: / /, action: ""},
+//    {name: "code", pattern: / /, action: ""},
+//    {name: "codeblock", pattern: / /, action: ""},
+    {name: "heading", pattern: /\[#* (.+)]/, action: "# $1"},
+//    {name: "hr", pattern: / /, action: ""},
+    {name: "image", pattern: /\[(.*[png|jpeg|jpg|bmp|svg|gif])\]/, action: "![image]($1)"},
+//    {name: "italic", pattern: / /, action: ""},
+//    {name: "link-includes-image", pattern: / /, action: ""},
+    {name: "link", pattern: /\[([^ ]*) (http.+)\]/, action: "[$1]($2)"},
+//    {name: "list-strong-style-text", pattern: / /, action: ""},
+    {name: "list", pattern: /^( +)/, action: "$1$1- "},
+//    {name: "paragraph", pattern: / /, action: ""},
+    {name: "strike", pattern: /\[- (.+)]/, action: "~~$1~~"},
+    {name: "strong-italic", pattern: /\[\*\/ (.+)]/g, action: "***$1***"},
+    {name: "strong", pattern: /\[\* (.+)]/g, action: "**$1**"},
+//    {name: "table", pattern: / /, action: ""},
+  ]
 
+  return input.split("\n").map(line=>{
+    var output = line
+    patterns.forEach(pattern=>{
+      if(pattern.action){
+        output = output.replace(pattern.pattern, pattern.action)
+      }
+      if(pattern.callback){
+        output = pattern.callback(output)
+      }
+    })
+    return output
+  }).join("\n")
+}
+
+
+module.exports = {
+  scrapboxToMarkdown: scrapboxToMarkdown,
+  precompileList: precompileList
+}
---
+++ test.js
@@ -0,0 +1,19 @@
+var util = require("./src/util")
+var test = require('tape');
+
+test('markdown', function (t) {
+    t.plan(7);
+    t.equal('**test**', util.scrapboxToMarkdown(`[* test]`));
+    t.equal('~~test~~', util.scrapboxToMarkdown(`[- test]`));
+    t.equal('# test', util.scrapboxToMarkdown(`[#### test]`));
+
+    //list
+    t.equal(`  - first`, util.scrapboxToMarkdown(` first`))
+    t.equal("  - first\n    - second", util.scrapboxToMarkdown(" first\n  second"))
+
+    t.equal("[test](http://www.pitecan.com/)", util.scrapboxToMarkdown("[test http://www.pitecan.com/]"))
+
+    t.equal("![image](http://www.pitecan.com/hello.png)", util.scrapboxToMarkdown("[http://www.pitecan.com/hello.png]"))
+
+});
+
