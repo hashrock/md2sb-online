77aa33dc0d70cc630647e83df8107f37c4906ab2
hashrock <hashrock@users.noreply.github.com>
2021-12-14 22:40:59 +0900

    dev: move to Vite, upgrade md2sb

--- src/util.js
+++ src/util.js
@@ -1,29 +1,4 @@
-
-/**
- * md2sbの不具合対応
- * 
- * List記法ネストの改行位置がおかしい問題があるため、
- * 前もってにsb形式のリストに変換してしまう。
- * 
- * markdownのリストのネスト判定はだいぶ面倒くさいことになっているので、
- * 真面目に対応せず、なんとなくネストっぽい見た目になっていればいいやということで、
- * リスト記号の前についてるスペースをそのまま残すことにした。
- */
-function precompileList(input){
-  var list = input.split("\n")
-  var convertedList = list.map(line => {
-    var pattern = /^([ \t]*)[-*+] /
-    var matched = pattern.exec(line)
-    if(matched){
-      return " " + line.replace(pattern, matched[1])
-    }else{
-      return line
-    }
-  })
-  return convertedList.join("\n")
-}
-
-function scrapboxToMarkdown(input){
+export function scrapboxToMarkdown(input){
   const converted = input.replace(/\t/g, " ")
 
   var patterns = [
@@ -59,8 +34,3 @@ function scrapboxToMarkdown(input){
   }).join("\n")
 }
 
-
-module.exports = {
-  scrapboxToMarkdown: scrapboxToMarkdown,
-  precompileList: precompileList
-}
